fc70a9ea112d4555cdf337370757b975648e03ac
Kade <kade.wang@freightamigo.com>
2024-01-10 15:31:39 +0800

    add not found page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CoreModule } from './core.module';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
         CommonModule,
-        CoreModule,
         RouterOutlet
-        ],
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
