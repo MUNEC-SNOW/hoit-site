d7177744a5d1869fb23063e5f46a8e381ad4a1c2
Kade <kade.wang@freightamigo.com>
2024-08-12 10:46:56 +0800

    add profile page

--- src/app/pages/info/info.routing.ts
+++ src/app/pages/info/info.routing.ts
@@ -12,6 +12,11 @@ export default [
                 loadComponent: () => import('./crush/crush.component').then(m => m.CrushComponent)
             },
             {
+                path: 'hoit-wang',
+                title: 'Hoit Wang',
+                loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent)
+            },
+            {
                 path: 'css-gallery',
                 title: 'CSS Gallery',
                 loadChildren: () => import('./css-gallery/css-gallery.routing')
